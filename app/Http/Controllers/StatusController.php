b117b82cef78c1ed42bbf9a9f562bce1eb2d3584
rossescl <rosses@gmail.com>
2023-11-10 17:08:18 -0300

    OK

--- app/Http/Controllers/StatusController.php
+++ app/Http/Controllers/StatusController.php
@@ -7,7 +7,8 @@ use Illuminate\Http\Request;
 class StatusController extends Controller
 {
     public function index() {
-        $rows = Status::select('Status.*')
+        $rows = Status::select('Status.*', 'Groups.Name as GroupName')
+                ->leftJoin('Groups','Groups.GroupID','=','Status.GroupID')
                 ->where('Status.Active',1)
                 ->orderBy('Status.Name','ASC')
                 ->get();
